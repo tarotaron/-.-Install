e64eb3d4523d23f638f5416b9a43fc69942051ad
tarotaron <suzu9095@gmail.com>
2020-05-22 13:51:50 +0900

    querySelectorAll

--- JsvaScript/JavaScriptBasics/main1.js
+++ JsvaScript/JavaScriptBasics/main1.js
@@ -8,3 +8,19 @@
 
 	setTimeout(update, 1000);
 }
+
+{
+	function update() {
+		// querySelector=見つかった最初の要素のみ
+		// document.querySelector('p').textContent = 'Chaged!';
+		// querySelectorAll=全ての対象
+		// document.querySelectorAll('p').textContent = 'Chaged!';
+		// 配列指定
+		// document.querySelectorAll('p')[2].textContent = 'Chaged!';
+		document.querySelectorAll('p').forEach((p, index) => {
+			p.textContent = `${index}番目のpです`;
+		});
+	}
+
+	setTimeout(update, 1000);
+}
